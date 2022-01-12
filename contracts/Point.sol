c0f24e83b51c2ab9e055deac879b6d8a5e0053d1
Ryan <81717668+RyanSea@users.noreply.github.com>
2022-01-11 20:18:56 -0800

    implemented ownable

--- contracts/Point.sol
+++ contracts/Point.sol
@@ -1,46 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Point is ERC20 {
-
-  address public owner;
-
-  event OwnerChanged(address indexed from, address to);
+contract Point is Ownable, ERC20 {
 
   constructor() 
     payable 
     ERC20("Inflection", "POINT") 
-  { 
-    owner = msg.sender;
-  }
-
-  function passOwnership(address inflection) 
-    public 
-    returns(bool)
   {
-    require(msg.sender == owner, 'Error, only owner can transfer minter role');
-    owner = inflection;
-
-    emit OwnerChanged(msg.sender, inflection);
-    return true;
+  
   }
 
-  function whoIsOwner() 
-    public 
-    view 
-    returns(address) 
-  {
-    return owner;
-  }
 
   function mint(address account, uint256 amount) 
     public 
+    onlyOwner
   {
-    require(msg.sender == owner, 'Error, sender is not owner');
+
 		_mint(account, amount);
+
 	}
 
 
